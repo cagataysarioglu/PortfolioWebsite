7fa68ded08adf5da2d18691e52a2ce8e6d012bf3
cagataysarioglu <sarioglucagatay@gmail.com>
2021-01-25 12:36:25 +0300

    Category class added.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('parent_id', 'name',)
+    list_display_links = ('parent_id', 'name',)
+    list_filter = ('parent_id', 'name',)
+    search_fields = ('parent_id', 'name',)
+    list_per_page = 20
+
+admin.site.register(Category, CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'amount', 'price', 'is_published',)
--- products/models.py
+++ products/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+class Category(models.Model):
+    parent_id = models.IntegerField(verbose_name="Kategori Nu.")
+    name = models.CharField(max_length=70, verbose_name="Kategori Adı")
+    description = models.TextField(blank=True, null=True, verbose_name="Kategori Açıklaması")
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Ürün Adı")
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="Ürün Adı")
     amount = models.CharField(max_length=50, verbose_name="Ürün Adedi")
     price = models.IntegerField(blank=True, null=True, verbose_name="Ürün Fiyatı")
     image = models.ImageField(blank=True, null=True, verbose_name="Ürün Görseli")
