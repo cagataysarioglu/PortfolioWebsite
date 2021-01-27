2a5e515264709b9abbb662b2d5f3550576737c8c
cagataysarioglu <sarioglucagatay@gmail.com>
2021-01-27 13:44:00 +0300

    Some settings done for deployement.

--- tantrade/settings.py
+++ tantrade/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
